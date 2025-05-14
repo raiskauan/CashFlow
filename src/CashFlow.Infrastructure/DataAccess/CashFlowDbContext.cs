d388e9806b4a5586ffdd45f81bb092b22c76c3ba
raiskauan <raiscesar5@gmail.com>
2025-05-14 15:03:55 -0300

    feat/refact: adicionado uma entidade Tag e migration para Tag, feito uma refatoração no DataAccess.

--- src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -8,5 +8,12 @@ public class CashFlowDbContext : DbContext
     public CashFlowDbContext(DbContextOptions options) : base(options) { }
     public DbSet<Expanse> Expanses { get; set; }
     public DbSet<User> User { get; set; }
-    
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Tag>().ToTable("Tags");
+    }
 }
